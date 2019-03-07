27cb0076e51403294805469e1a9d583262f9e574
Amita <amita.parikh@gmail.com>
2019-03-07 18:54:03 +0000

    Pythagorean practise

---
+++ pythagoreantheorem.js
@@ -0,0 +1,18 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(num) {
+  // console.log(num);
+  var power = Math.pow(num.x, 2) + Math.pow(num.y, 2);
+
+
+  // console.log(power);
+  return Math.sqrt(power);
+  // return sqrt
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
