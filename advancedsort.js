ce43d11dd22740a0cc03a5d355cccc8075fd3828
Amita <amita.parikh@gmail.com>
2019-03-07 20:06:38 +0000

    Custom Callback functions

---
+++ advancedsort.js
@@ -0,0 +1,28 @@
+// var students = [
+//   { id: 1, name: "bruce",    age: 40 },
+//   { id: 2, name: "zoidberg", age: 22 },
+//   { id: 3, name: "alex",     age: 22 },
+//   { id: 4, name: "alex",     age: 30 }
+// ];
+
+// students.sort();
+
+var students = [
+   { id: 1, name: "bruce", age: 40 },
+   { id: 2, name: "zoidberg", age: 22 },
+   { id: 3, name: "alex", age: 22 },
+   { id: 4, name: "alex", age: 30 }
+];
+
+students.sort(function (a, b) {
+   if (a.name === b.name) {
+       return b.age - a.age;
+   }
+   return a.name > b.name;
+
+});
+
+console.log(students);
+// students();
+
+//question to ask and consider: why is a before b?
