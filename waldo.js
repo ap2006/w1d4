7c28337d1d2e201a50e25c49b1dee5f519ec6606
Amita <amita.parikh@gmail.com>
2019-03-07 17:39:40 +0000

    Print the index of Waldo when found

---
+++ waldo.js
@@ -0,0 +1,13 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(element, i) {
+    if (element === "Waldo") {
+      found(i)
+    }
+  })
+}
+function actionWhenFound(index) {
+  console.log("Found him at index " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
